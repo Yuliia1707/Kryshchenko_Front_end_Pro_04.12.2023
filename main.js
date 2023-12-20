1697834756c6246f8bb34e1a88908fd5ffc7ff21
Yuliia <yuliiakryshchenko@gmail.com>
2023-12-20 19:54:11 +0200

    dom commit

--- main.js
+++ main.js
@@ -22,54 +22,66 @@
 
 //--------------------------------------------------------------//
 
-function showInformation() {
-    const yearOfBirth = prompt("Введіть свій рік народження:");
-    if (yearOfBirth === null) {
-        alert("Шкода, що Ви не захотіли ввести свій рік народження.");
-        return;
-    }
+// function showInformation() {
+//     const yearOfBirth = prompt("Введіть свій рік народження:");
+//     if (yearOfBirth === null) {
+//         alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+//         return;
+//     }
 
-    const city = prompt("Введіть місто, в якому ви живете:");
-    if (city === null) {
-        alert("Шкода, що Ви не захотіли ввести своє місто.");
-        return;
-    }
+//     const city = prompt("Введіть місто, в якому ви живете:");
+//     if (city === null) {
+//         alert("Шкода, що Ви не захотіли ввести своє місто.");
+//         return;
+//     }
 
-    const favoriteSport = prompt("Введіть свій улюблений вид спорту:");
-    if (favoriteSport === null) {
-        alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
-        return;
-    }
-
-
-    const currentYear = new Date().getFullYear();
-    const age = currentYear - parseInt(yearOfBirth);
+//     const favoriteSport = prompt("Введіть свій улюблений вид спорту:");
+//     if (favoriteSport === null) {
+//         alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+//         return;
+//     }
 
 
-    const capitals = ["Київ", "Вашингтон", "Лондон"];
-    let message = "";
-    if (capitals.includes(city)) {
-        const countries = {
-            "Київ": "Україна",
-            "Вашингтон": "США",
-            "Лондон": "Велика Британія"
-        };
-        message = `Ти живеш у столиці ${countries[city]}`;
-    } else {
-        message = `Ти живеш у місті ${city}`;
-    }
+//     const currentYear = new Date().getFullYear();
+//     const age = currentYear - parseInt(yearOfBirth);
 
 
-    const sportsChampions = {
-        "футбол": "Ліонель Мессі",
-        "баскетбол": "Леброн Джеймс",
-        "теніс": "Серена Вільямс"
-    };
+//     const capitals = ["Київ", "Вашингтон", "Лондон"];
+//     let message = "";
+//     if (capitals.includes(city)) {
+//         const countries = {
+//             "Київ": "Україна",
+//             "Вашингтон": "США",
+//             "Лондон": "Велика Британія"
+//         };
+//         message = `Ти живеш у столиці ${countries[city]}`;
+//     } else {
+//         message = `Ти живеш у місті ${city}`;
+//     }
 
-    if (sportsChampions.hasOwnProperty(favoriteSport)) {
-        message += `\nКруто! Хочеш стати як ${sportsChampions[favoriteSport]}?`;
-    }
 
-    alert(`Ваш вік: ${age}\n${message}`);
-}
-showInformation();
+//     const sportsChampions = {
+//         "футбол": "Ліонель Мессі",
+//         "баскетбол": "Леброн Джеймс",
+//         "теніс": "Серена Вільямс"
+//     };
+
+//     if (sportsChampions.hasOwnProperty(favoriteSport)) {
+//         message += `\nКруто! Хочеш стати як ${sportsChampions[favoriteSport]}?`;
+//     }
+
+//     alert(`Ваш вік: ${age}\n${message}`);
+// }
+// showInformation();
+
+
+//--------------------------------------------------------------//
+
+
+document.getElementById('textField').addEventListener('focus', function () {
+    document.getElementById('infoDiv').style.display = 'block';
+});
+
+document.getElementById('textField').addEventListener('blur', function () {
+    document.getElementById('infoDiv').style.display = 'none';
+});
